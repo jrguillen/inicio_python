9e28a17a4ebfd1f4275a5bf804769cf6069970b1
Jose Guillen <joseguillennavas@gmail.com>
2017-09-23 10:15:10 -0500

    Enunciado del ejercicio Bienvenida al sistem v1.0

--- prog_Bienv.py
+++ prog_Bienv.py
@@ -1,3 +1,6 @@
+# Escribe un programa que use raw_input para pedirle al usuario su
+# nombre y luego darle la bienvenida.
+
 Nomb_user = raw_input('Introduzca su Nombre>>> ')
 
 print Nomb_user,'Bienvenido al Sistema...'
