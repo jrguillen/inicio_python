cc427ffb35c76624b4cf6b0506b398d2c05cde78
Jose Guillen <joseguillennavas@gmail.com>
2017-09-23 12:28:34 -0500

    Modificacion el Program_salario se modifica para usar una funcion

--- calculo_salario.py
+++ calculo_salario.py
@@ -2,18 +2,21 @@
 # 1.5 veces la tarifa horaria para todas las horas trabajadas que excedan de 40.
 
 
-
 try:
 	num_horas = float(raw_input('Ingrese las horas trabajas>>> '))
 	tarifa_horas = float(raw_input('Ingrese el costo de la hora trabajada>>> '))
-	if num_horas > 40:
-		horas_extra = num_horas - 40
-		tarifa_horas_extra = (tarifa_horas * 1.5) * horas_extra
-		salario = 40 * tarifa_horas
-		salario_neto = salario + tarifa_horas_extra
-		print 'El salario a cobrar es>>>', salario_neto
-	else:
-		salario_neto = num_horas * tarifa_horas
-		print 'El salario neto a cobrar es:',salario_neto
+	def calculo_salario(num_horas, tarifa_horas):
+		if num_horas > 40:
+			horas_extra = num_horas - 40
+			tarifa_horas_extra = (tarifa_horas * 1.5) * horas_extra
+			salario = 40 * tarifa_horas
+			salario_neto = salario + tarifa_horas_extra
+			print 'El salario a cobrar es>>>', salario_neto
+		else:
+			salario_neto = num_horas * tarifa_horas
+			print 'El salario neto a cobrar es:',salario_neto
+
+	calculo_salario(num_horas, tarifa_horas)
+
 except:
 	print '<<< Solo Puedes Ingresar valores numericos>>>'
