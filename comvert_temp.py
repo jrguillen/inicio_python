b9c063cfb89b6c9cfdc7e9efbb020a90a927ef8b
Jose Guillen <joseguillennavas@gmail.com>
2017-09-23 10:52:25 -0500

    Comversor de temperatura muy basico v1.0

---
+++ comvert_temp.py
@@ -0,0 +1,10 @@
+### COVERTIDOR DE TEMPERATURA Celsius A Fahrenheit #####
+
+
+
+temp_c = float(raw_input('Introduzca grados Celsius>>> '))
+
+temp_f = (temp_c * 1.8) + 32
+
+
+print temp_f,'Grados Fahrenheit'
